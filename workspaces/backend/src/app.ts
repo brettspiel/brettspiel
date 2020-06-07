b5628a0d5527237a6d859ccf8ceceaed8e26816f
airtoxin <airtoxin@icloud.com>
2020-06-07 14:34:22 +0900

    Allow cors request in socket.io connection

--- workspaces/backend/src/app.ts
+++ workspaces/backend/src/app.ts
@@ -9,7 +9,18 @@ import { TypedSocketEvent } from "./TypedSocketEvent";
 
 const app = express();
 export const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+  handlePreflightRequest: (req: any, res: any) => {
+    const headers = {
+      "Access-Control-Allow-Headers":
+        "Content-Type, Authorization, x-user-id, x-secret-token",
+      "Access-Control-Allow-Origin": req.headers.origin,
+      "Access-Control-Allow-Credentials": true,
+    };
+    res.writeHead(200, headers);
+    res.end();
+  },
+});
 
 app.use(express.json());
 app.use(cors());
