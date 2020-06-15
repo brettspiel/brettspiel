9bf42e8d1e082a0d5ddb811999b48a191e244698
airtoxin <airtoxin@icloud.com>
2020-06-15 21:32:07 +0900

    WIP

--- workspaces/backend/src/app.ts
+++ workspaces/backend/src/app.ts
@@ -1,19 +1,10 @@
 import express from "express";
-import http from "http";
-import socketIo from "socket.io";
 import cors from "cors";
 import { healthcheckRoute } from "./controllers/healthcheck";
 import { usersRoute } from "./controllers/users";
-import { loungeSocket } from "./controllers/loungeSocket";
-import { ServerSocket } from "@brettspiel/typed-socket/lib/ServerSocket";
-import { socketIoPreflightRequestHandler } from "./middlewares/socketIoPreflightRequestHandler";
-import { socketAuth } from "./middlewares/socketAuth";
+import ExpressWs from "express-ws";
 
-const app = express();
-export const server = http.createServer(app);
-const io = socketIo(server, {
-  handlePreflightRequest: socketIoPreflightRequestHandler,
-});
+const { app } = ExpressWs(express());
 
 app.use(express.json());
 app.use(cors());
@@ -21,7 +12,8 @@ app.use(cors());
 app.use("__healthcheck", healthcheckRoute);
 app.use("/users", usersRoute);
 
-io.of("/lounge").on(
-  "connection",
-  socketAuth((socket, user) => loungeSocket(new ServerSocket(socket), user))
-);
+app.ws("/lounge", (ws) => {
+  ws.on("message", (message) => {
+    ws.send(message);
+  });
+});
